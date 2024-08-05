e96ec157bc62b15330973c82fde865c4b2ed60cb
yuuki0217 <Growthenvironment19oka@gmail.com>
2024-08-05 21:01:27 +0900

    商品出品機能　実装　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -24,4 +24,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_origin_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: 'must be greater than or equal to 300 and less than or equal to 9999999' }
+  validates :price, numericality: { only_integer: true, message: 'is not a number' }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -96,6 +96,30 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
+
+      it '価格が、¥300以上でない場合、登録できない' do
+        @item.price = '30'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300 and less than or equal to 9999999')
+      end
+
+      it '価格が、¥9,999,999以下でない場合、登録できない' do
+        @item.price = '99999999'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300 and less than or equal to 9999999')
+      end
+
+      it '価格が半角数値では登録できない' do
+        @item.price = '１２３４５'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it 'userが紐づいていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
