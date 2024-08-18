dab914a23758ca37d5d493922c0b9f1b694263c9
yuuki0217 <Growthenvironment19oka@gmail.com>
2024-08-18 21:07:56 +0900

    商品購入機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,7 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return if current_user == @item.user
+    return if current_user != @item.user
 
     redirect_to root_path
   end
